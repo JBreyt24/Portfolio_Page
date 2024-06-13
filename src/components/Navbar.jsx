148e2d19f53fac28958ea3065ca5a0829d757132
Josh Breytspraak <jbreyt24@gmail.com>
2024-06-12 22:17:54 -0500

    updated home page to use next link instead of anchor tag

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,9 +10,9 @@ export default function Navbar() {
             <div className="container mx-auto px-6 py-3 flex justify-between items-center">
                 <div className="text-xl font-bold">My Portfolio</div>
                 <div className="flex space-x-4">
-                    <a href="#about" className="hover:text-blue-500">About</a>
-                    <a href="#projects" className="hover:text-blue-500">Projects</a>
-                    <a href="#contact" className="hover:text-blue-500">Contact</a>
+                    <Link href="#about" className="hover:text-blue-500">About</Link>
+                    <Link href="#projects" className="hover:text-blue-500">Projects</Link>
+                    <Link href="#contact" className="hover:text-blue-500">Contact</Link>
                 </div>
             </div>
         </nav>
