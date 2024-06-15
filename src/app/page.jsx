8f27f90bdfb550cdacbe6a8201b84ad1f5ff8d30
Josh Breytspraak <jbreyt24@gmail.com>
2024-06-15 01:56:46 -0500

    added mobile query and updated ui

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -48,7 +48,7 @@ export default function Home() {
 
           {/* Home Section */}
 
-          <section id="home" className="min-h-screen flex flex-col items-center justify-center bg-secondary w-full py-20 text-white obelisk">
+          <section id="home" className="min-h-screen flex flex-col items-center justify-center bg-secondary py-20 text-white obelisk">
             <div className="obelisk-content">
               <h2 className="text-4xl font-bold mb-4">Welcome to my Portfolio Website!</h2>
             </div>
@@ -56,7 +56,7 @@ export default function Home() {
 
           {/* About Section */}
 
-          <section id="about" className="min-h-screen flex flex-col items-center justify-center bg-secondary w-full py-20 text-white obelisk">
+          <section id="about" className="min-h-screen flex flex-col items-center justify-center bg-secondary py-20 text-white obelisk">
             <div className="obelisk-content">
               <h2 className="text-4xl font-bold mb-4">About Me</h2>
               <p className="mt-4 text-xl max-w-4xl">Hi, I'm Josh Breytspraak. I'm a Full Stack Web Developer with experience in various technologies. I love building web applications and learning new technologies.</p>
@@ -65,19 +65,19 @@ export default function Home() {
 
           {/* Projects Section */}
 
-          <section id="projects" className="min-h-screen flex flex-col items-center justify-center bg-secondary w-full py-20 text-white obelisk">
+          <section id="projects" className="min-h-screen flex flex-col items-center justify-center bg-secondary py-20 text-white obelisk">
             <div className="obelisk-content">
               <h2 className="text-4xl font-bold mb-4">Projects</h2>
-              <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-                <div className="w-96 p-6 mt-6 text-left border rounded-xl bg-black text-white obelisk">
-                  <h3 className="text-2xl font-bold">Project 1</h3>
+              <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 py-20">
+                <div className="p-6 mt-6 text-left border rounded-xl bg-black text-white project-card">
+                  <h3 className="text-2xl font-bold">ClimaTrac Weather App</h3>
                   <p className="mt-4 text-xl">Description of Project 1.</p>
                 </div>
-                <div className="w-96 p-6 mt-6 text-left border rounded-xl bg-black text-white obelisk">
+                <div className="p-6 mt-6 text-left border rounded-xl bg-black text-white project-card">
                   <h3 className="text-2xl font-bold">Project 2</h3>
                   <p className="mt-4 text-xl">Description of Project 2.</p>
                 </div>
-                <div className="w-96 p-6 mt-6 text-left border rounded-xl bg-black text-white obelisk">
+                <div className="p-6 mt-6 text-left border rounded-xl bg-black text-white project-card">
                   <h3 className="text-2xl font-bold">Project 3</h3>
                   <p className="mt-4 text-xl">Description of Project 3.</p>
                 </div>
@@ -87,11 +87,11 @@ export default function Home() {
 
           {/* Contact Section */}
 
-          <section id="contact" className="min-h-screen flex flex-col items-center justify-center bg-secondary w-full py-20 text-white obelisk">
+          <section id="contact" className="min-h-screen flex flex-col items-center justify-center bg-secondary py-20 text-white obelisk">
             <div className="obelisk-content">
-              <h2 className="text-4xl font-bold mb-4">Contact</h2>
-              <p className="mt-4 text-xl max-w-4xl">Feel free to get in touch with me.</p>
-              <form className="mt-8 space-y-4 w-full max-w-md">
+              <h2 className="text-4xl font-bold mb-4">Let's Connect!</h2>
+              <p className="mt-4 text-xl max-w-4xl"></p>
+              <form className="flex flex-col items-center justify-center mt-8 space-y-4 w-full max-w-md">
                 <div>
                   <label htmlFor="name" className="block text-sm font-medium text-white">Name</label>
                   <input type="text" id="name" className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-primary focus:border-primary sm:text-sm bg-black text-white" />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,36 +2,78 @@
 import { FaGithub, FaLinkedin, FaDiscord } from 'react-icons/fa';
 import Link from 'next/link';
 import React from 'react';
+import { AlignJustify } from 'lucide-react'; // import this for the hamburger icon
+
 
 
 export default function Navbar() {
     return (
-        <nav className="w-full fixed top-0 left-0 bg-red-black-gradient p-4 z-50">
-            <ul className="flex justify-center space-x-8 text-white">
-                <li>
-                    <a href="#home" className="hover:text-primary">Home</a>
-                </li>
-                <li>
-                    <a href="#about" className="hover:text-primary">About</a>
-                </li>
-                <li>
-                    <a href="#projects" className="hover:text-primary">Projects</a>
-                </li>
-                <li>
-                    <a href="#contact" className="hover:text-primary">Contact</a>
-                </li>
-                <li className="flex items-center space-x-4">
-                    <a href="https://github.com/JBreyt24" target="_blank" rel="noopener noreferrer" className="hover:text-primary">
-                        <FaGithub className="w-6 h-6" />
-                    </a>
-                    <a href="https://www.linkedin.com/in/josh-breytspraak-251624b0/" target="_blank" rel="noopener noreferrer" className="hover:text-primary">
-                        <FaLinkedin className="w-6 h-6" />
-                    </a>
-                    <a href="https://discordapp.com/users/279118126027898890" target="_blank" rel="noopener noreferrer" className="hover:text-primary">
-                        <FaDiscord className="w-6 h-6" />
-                    </a>
-                </li>
-            </ul>
-        </nav>
+        <>
+
+            {/* Desktop Navbar */}
+
+            <nav className="hidden lg:flex fixed top-0 left-0 bg-red-black-gradient p-4 z-50 w-full justify-center">
+                <ul className="flex justify-center space-x-8 text-white">
+                    <li>
+                        <a href="#home" className="hover:text-primary">Home</a>
+                    </li>
+                    <li>
+                        <a href="#about" className="hover:text-primary">About</a>
+                    </li>
+                    <li>
+                        <a href="#projects" className="hover:text-primary">Projects</a>
+                    </li>
+                    <li>
+                        <a href="#contact" className="hover:text-primary">Contact</a>
+                    </li>
+                    <li className="flex items-center space-x-4">
+                        <a href="https://github.com/JBreyt24" target="_blank" rel="noopener noreferrer" className="hover:text-primary">
+                            <FaGithub className="w-6 h-6" />
+                        </a>
+                        <a href="https://www.linkedin.com/in/josh-breytspraak-251624b0/" target="_blank" rel="noopener noreferrer" className="hover:text-primary">
+                            <FaLinkedin className="w-6 h-6" />
+                        </a>
+                        <a href="https://discordapp.com/users/279118126027898890" target="_blank" rel="noopener noreferrer" className="hover:text-primary">
+                            <FaDiscord className="w-6 h-6" />
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+
+            {/* Mobile Navbar */}
+
+            <nav className="lg:hidden flex fixed top-0 left-0 bg-red-black-gradient p-4 z-50 w-full justify-left">
+                <ul className="flex justify-center space-x-8 text-white">
+                    <div className="relative group">
+                        <a className="text-md active:font-bold hover:text-blue-400 text-lg text-center w-full no-underline sm:w-auto sm:pr-4 py-2 sm:py-1" >
+                        <AlignJustify />
+                        </a>
+                        <div className="absolute z-10 hidden bg-grey-200 group-hover:block">
+                            <div className="px-2 pt-2 pb-4 bg-red-200 shadow-lg">
+                                <div className="dropdown-menu">
+                                    <ul>
+                                        <li><a href="#home" className=" dropdown-item">Home </a></li>
+                                        <li><a href="#about" className=" dropdown-item">About </a></li>
+                                        <li><a href="#projects" className=" dropdown-item">Projects </a></li>
+                                        <li><a href="#contact" className=" dropdown-item">Contact </a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                        <li className="flex items-center space-x-4">
+                            <a href="https://github.com/JBreyt24" target="_blank" rel="noopener noreferrer" className="hover:text-primary">
+                                <FaGithub className="w-6 h-6" />
+                            </a>
+                            <a href="https://www.linkedin.com/in/josh-breytspraak-251624b0/" target="_blank" rel="noopener noreferrer" className="hover:text-primary">
+                                <FaLinkedin className="w-6 h-6" />
+                            </a>
+                            <a href="https://discordapp.com/users/279118126027898890" target="_blank" rel="noopener noreferrer" className="hover:text-primary">
+                                <FaDiscord className="w-6 h-6" />
+                            </a>
+                        </li>
+                </ul>
+            </nav>
+        </>
     );
 }
